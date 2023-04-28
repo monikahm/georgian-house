4a4a49ea0279ec5d3a09824ed106761899af1295
Monika Muzyk <monika.muzyk@sparebank1.no>
2023-04-28 16:51:43 +0200

    Setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,326 @@
-
-import './App.css'
+import MTEVino from './assets/logo.svg'
+import "./App.css";
 
 function App() {
-
   return (
-    <> 
-      <h1>Magnat Wina</h1>
+    <>
+    <div className="wine__full-height wine__center">
+
+      <h1 >Georgian House</h1>
+      <img src={MTEVino} alt='MTEVino Logo'/>
+    </div>
+
+      <h2 className=" ">Georia - Cradle of Wine</h2>
+      <div className="wine-history wine__full-height">
+        <p>
+          Statue of “Tamada” from VII century B.C. discovered in Vani city and
+          now exhibited at National Museum of Georgia.GEORGIA -CRADLE OF
+          WINEAccording to archeologic data, Georgian’s rela-tion with vine and
+          wine is observed from early stag-es. Qvevri widely used in Georgia for
+          winemaking is similar to vessels from Neolithic area. What proves,
+          that wine culture in this area has long historic routes and defines
+          continuity of this process. It is very im-portant that we have not
+          only one episode, but the entire picture of the story. We started wine
+          produc-tion in ancient times and continue this activity now-adays.
+        </p>
+        <p>
+          Study of monuments of Neolithic area on the ter-ritory of Georgia (VI
+          century B.C.) was initiated in 60s of XX century. During this process
+          wine vessel was discovered, which allowed researchers society of XX
+          century to assume, that Georgia was the moth-erland of wine. In 1999,
+          “Vinopolis” was opened in London, where Georgian corner was
+          established. This exposition was named- “Georgia- Motherland of Wine”.
+          Here, based on samples stored in State Museum of Georgia history of
+          Georgian wine was re-produced.
+        </p>
+        <p>
+          Assumption, that Georgia is the motherland of wine was first mentioned
+          by famous writer Hugh Johnson in his book “History of Wine”, which was
+          published in 1989. Later, similar assumption was made by acknowledge
+          wine history researcher, pro-fessor of Pennsylvania university-
+          Patrick McGovern, who traveled Georgia in 1998. In 2003 he published
+          his book “Ancient Wine”, where he mentioned, that motherland of wine
+          is possibly Caucasus area. His assumptions were based on archeological
+          discoveries mentioned above and grape seeds discovered. How-ever,
+          unfortunately international society did not ap-prove this discovery,
+          as vessels and seeds could not be dated for sure.
+        </p>
+        <p>
+          In 2006, national museum was tasked to renew the exposition at
+          “Vinopolis”. From this time, research of wine related topics and
+          sharing results with in-ternational society has become our major
+          direction. Also, from the same period of time archaeological studies
+          of Neolithic monuments was renewed. Re-newal of the studies aimed at
+          support of wine culture studies. Statue of “Tamada” from VII century
+          B.C. discovered in Vani city and now exhibited at Nation-al Museum of
+          Georgia has become its symbol. In 2008, professor Patrick McGovern
+          visit Georgia again and held lecture on study of chemical compo-sition
+          of ancient wine. Several times, symposium for wine community,
+          including Qvevri was organized.
+        </p>
+        <p>
+          From 2014, “Georgian Wine” association and Na-tional Wine Agency of
+          Georgia initiated an inter-national project- “Georgian vine and wine
+          culture study”. Along with Georgian scientists, internation-al
+          scientist from Pennsylvania, Montpelier, Milan, Copenhagen, Toronto
+          Universities, Israel Weisman university and Montpelier National
+          Institute of Ag-ricultural Research (INRA) also participated in the
+          project. In the scope of the project, professor of Pennsylvania
+          university- Patrick McGovern facili-tated biomolecular study. Clay
+          vessels discovered during archeological study of Neolithic area
+          mon-uments in “Gadachrili Gori” and “Shulaveris Gori” located in
+          Marneuli region, proved presence of or-ganic acids characteristic to
+          wines. These acids are the markers of wines produced from Vitis
+          Vinefera
+        </p>
+      </div>
+
+      <div className="wine-white wine__full-height  wine__center">
+        <h2>Rkatsiteli</h2>
+        <h3>Dry white</h3>
+        <p>
+          The wine is made of Rkatsiteli grape variety planted in Kakheti
+          Region.It has light straw color. Distinguished by the pleasant fruit
+          tones and cheerful, harmonious taste.
+        </p>
+        <p> Served with: Cheese, vegetable and sea products.</p>
+        <p>Serving temperature: +16- +18.</p>
+        <p>Storing temperature: +12 - +16.</p>
+      </div>
+
+      <div className="wine-white wine__full-height">
+        <h2>Alazani Valley</h2>
+        <h3>Medium-sweet white</h3>
+        <p>
+          The wine made of Rkatsiteli grape variety, grown in Kakheti Region,
+          planted on Alazani Valley. It has light-straw color and pleasant
+          harmonious taste
+        </p>
+        <p>Served with: Cheese and Fruit. </p>
+        <p>Serving temperature: +8 - +10. </p>
+        <p>Storing Temperature: +12 - +16. </p>
+      </div>
+
+      <div className="wine-red wine__full-height">
+        <h2>Alazani Valley</h2>
+        <h3>Medium-sweet red</h3>
+        <p>
+          The wine is made of Saperavi grape variety, grown in Kakheti Region,
+          planted on Alazani Valley. It has dark pomegranate color, with gentle,
+          pleasant avor and aroma of the berries.
+        </p>
+        <p>Served with: Cheese, Vegetables and Sea Products. </p>
+        <p>Serving temperature : +8 - +10. </p>
+        <p>Storing temperature: +12 - +16. </p>
+      </div>
+
+      <div className="wine-red wine__full-height">
+        <h2>Saperavi</h2>
+        <h3>Dry red</h3>
+        <p>
+          The wine is made of Saperavi s grape variety, planted in Kakheti
+          Region. It has dark pomegranate color. Domains black currant and
+          blackberry tones in varieties flavor. Distinguished by the feathery,
+          harmonious taste.
+        </p>
+        <p>Served with: Meat and spicy dishes. </p>
+        <p>Serving temperature: +16- +18. </p>
+        <p>Storing temperature: +12- +16. </p>
+      </div>
+
+      <div className="wine-red wine__full-height">
+        <h2>Pirosmani</h2>
+        <h3>Medium-dry red</h3>
+        <p>
+          The wine is made of Saperavi s grape variety, planted in Kakheti
+          Region. It has dark pomegranate color. Domains black currant and
+          blackberry tones in varieties avor. Distinguished by the feathery,
+          harmonious taste.{" "}
+        </p>
+        <p>Served with: Meat and spicy dishes. </p>
+        <p>Serving temperature: +16- +18. </p>
+        <p>Storing temperature: +12- +16. </p>
+      </div>
+
+      <div className="wine-red wine__full-height">
+        <h2>Saperavi Khashmi</h2>
+        <h3>Dry red</h3>
+        <p>
+          Appelacion controlled red wine is produced in Kakheti Region by
+          Saperavi grape, grown in Khashmi micro zone. The wine has dark ruby
+          color. Characterized with the distinct varieties avor and perfect
+          velvet bouquet.
+        </p>
+        <p>Served with : Meat, cheese and vegetables. </p>
+        <p>Serving temperature: +16- +18. </p>
+        <p>Storing temperature: +12- +16. </p>
+      </div>
+
+      <div className="wine-red wine__full-height">
+        <h2>Mukuzani</h2>
+        <h3>Dry red</h3>
+        <p>
+          Appelacion controlled red wine is produced in Kakheti Region, from
+          Saperavi grape, grown in Mukuzani micro zone. Distinguished by the
+          dark ruby color. Characterized by the distinct varieties of flavor and
+          velvet bouquet.
+        </p>
+        <p>Served with: Meat, cheese and vegetable. </p>
+        <p>Serving temperature: +16- +18. </p>
+        <p>Storing temperature: +12- +16. </p>
+      </div>
+
+      <div className="wine-red wine__full-height">
+        <h2>Kindzmarauli</h2>
+        <h3>Medium-sweet red</h3>
+        <p>
+          Appelacion controlled red, naturally medium sweet wine of origin. It
+          is made of selected Saperavi grape variety, grown in Kakheti Region,
+          Kindzmarauli micro zone. It has dark pomegranate color. Domains black
+          currant and blackberry tones in varieties avor. Distinguished by the
+          feathery, harmonious taste.
+        </p>
+        <p>Served with: Meat and spicy dishes.</p>
+        <p>Serving temperature: +8 - +10.</p>
+      </div>
+
+      <h2>Qvevri Wine</h2>
+      <div className="wine-history wine__full-height">
+        <p>
+          In Georgia, winemaking in Qvevri started 8,000 years ago, and this
+          tradition is still maintained. In 2013 UNESCO has added the
+          traditional Georgian method of winemaking in Qvevri in the list of the
+          World Intangible Cultural Heritage. This points to the uniqueness and
+          significance of this method, and it is a message to the whole world
+          that wine is an integral part of the ancient Georgian culture. This
+          recognition is important for raising the awareness of Qvevri wines and
+          supports the worldwide promotion of the Georgian wines.
+          <p>Storing temperature: +12- +16. </p>
+        </p>
+        <p>
+          Qvevri is a unique clay vessel for making and storing wine. The oldest
+          Qvevri type vessels found in Georgia belong to the VI-V millennium BC.
+          The current Georgian Qvevri’s shape was formed in the III-II
+          millennium. Prior to this time, there were widespread mainly small
+          Qvevris that were not larger than 1-1.5 meters in height and had a
+          flat foot and wide belly. At present the most widespread are
+          egg-shaped Qvevris. The Qvevri’s capacity starts from several hundred
+          liters and reaches several tons. Kakheti is especially distinguished
+          for its large Qvevris, where you can find Qvevris with capacity of 6-8
+          thousand liters. The most common are 1-2-tones Qvevris.
+        </p>
+        <p>
+          Winemaking process in Qvevri generally involves fermentation of sweet
+          grape juice with a certain amount of pomace (mash of pips, skins and
+          stalks - called chacha in Georgian). The first and most important rule
+          of winemaking in Qvevri is to leave grape juice together with its
+          pomace, both during fermentation and after it.
+        </p>
+        <p>
+          Winemaking technology differs in different parts of Georgia. For
+          example, in Kakheti the alcoholic fermentation includes the total
+          amount of chacha (pomace), while fermentation by Imeretian method
+          involves adding up to a third of the total chacha to the grape juice
+          in the Qvevri.
+        </p>
+        <p>
+          For determination of the period for wine making with chacha, it is
+          important to take into account the grape variety, the duration of
+          alcoholic fermentation itself, environmental conditions, etc.
+          Generally, the red wine stays in Qvevri only during the fermentation
+          period, it can be 7-10 days, maximum 2 weeks. In the case of white
+          grapes, the wine stays with chacha until spring. The white wine, which
+          was stored with chacha for a long period, has a dark brown, golden or
+          tea color. It is transparent and sparkling, characterized by fruit
+          tones and a moderate content of tannins and is naturally stable. Clay
+          used for Qvevri-making contains limestone and small amounts of
+          precious metals - gold, silver and copper. Lime, which reacts with the
+          acid of wine, strengthens the walls of the Qvevri on the one hand, and
+          on the other hand - acts as a natural antiseptic, which is very
+          important in winemaking, where up to 400 species of bacteria are
+          known.
+        </p>
+        <p>
+          It takes an average of 3 months to make a Qvevri. Most of the time is
+          spent on formation and drying. After Qvevri is formed, it is placed in
+          the oven, where the most difficult stage of roasting begins.
+        </p>
+        <p>
+          The temperature inside the Qvevri buried in the ground does not change
+          and keeps constant at 13 -15° -the temperature required for wine
+          fermentation. In the Qvevri the chemical processes for wine making are
+          going naturally and in the relevant chronological order, at a factory
+          production for this are necessary special equipment and additives.
+        </p>
+        <p>
+          During the wine fermentation, it needs frequent mixing - 4-5 times a
+          day. At the end of fermentation, grape skins, stalks and pips begin to
+          sink and accumulate on the bottom of the Qvevri. Under the pressure
+          the chacha covers by the wine sediment, which separates it from the
+          wine.
+        </p>
+        <p>
+          In one of the main winemaking regions of Georgian - in Kakheti,
+          Qvevris are buried under ground in a wine cellar, in western Georgia -
+          Qvevris are hermetically sealed and kept at the open air.
+        </p>
+      </div>
+      <div className="wine-white wine__full-height">
+        <h2>Rkatsiteli</h2>
+        <h3>Dry white</h3>
+        <p>
+          The wine is made of Rkatsiteli grape by using the traditional Georgian
+          method of winemaking in Qvevri. The wine has dark amber color.
+          Characterized by tones of the dry fruit, exotic spices and harmonious
+          taste
+        </p>
+        <p> Served with: Vegetable, cheese and meat. </p>
+        <p>Serving temperature: +16 - +18. </p>
+        <p>Storing temperature: +12- +16.</p>
+      </div>
+      <div className="wine-white wine__full-height">
+        <h2>Rkatsiteli-Mtsvane</h2>
+        <h3>Dry white</h3>
+        <p>
+          The wine is made of Rkatsiteli grape by using the traditional Georgian
+          method of winemaking in Qvevri. The wine has dark amber color.
+          Characterized by tones of the dry fruit, exotic spices and harmonious
+          taste
+        </p>
+        <p>Served with: Vegetable, cheese and meat. </p>
+        <p>Serving temperature: +16 - +18. </p>
+        <p>Storing temperature: +12- +16.</p>
+      </div>
+
+      <div className="wine-white wine__full-height">
+        <h2>Mtsvane</h2>
+        <h3>Dry white</h3>
+        <p>
+          The wine is made of Kakhetian Mtsvane grape by using the traditional
+          Georgian method of winemaking in Qvevri. It has dark amber color.
+          Characterized by tones of the dry fruit, exotic spices and harmonious
+          taste.
+        </p>
+        <p>Served with: Vegetable, cheese and meat. </p>
+        <p>Serving temperature: +16 - +18. </p>
+        <p>Storing temperature: +12- +16.</p>
+      </div>
+
+      <div className="wine-red wine__full-height">
+        <h2>Saperavi</h2>
+        <h3>Dry red</h3>
+        <p>
+          The wine is made of Khashmi Saperavi grape variety, by using the
+          Georgian traditional method of winemaking in Qvevri. It has dark ruby
+          color. Distinguished by the long-lasting taste and aromas of the ripe
+          berries.
+        </p>
+        <p>Served with : Spicy dishes and well as alloyed with meat. </p>
+        <p>Serving temperature: +16 - +18. </p>
+        <p>Storing temperature: +12- +16.</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
